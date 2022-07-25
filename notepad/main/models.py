6087b554f3a4ae100749903094c785528932c499
DELL <nickorovkinwork@gmail.com>
2022-07-25 18:21:18 +0300

    Models init

--- notepad/main/models.py
+++ notepad/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Notes(models.Model):
+	noteid = models.CharField('NoteID', max_length = 10)
+	title = models.CharField('Название', max_length = 30)
+	author = models.CharField('Автор', max_length = 30)
+	text = models.TextField('Статья')
+	date = models.DateTimeField(null = True)
+
+	def __str__(self):
+		return f'{self.noteid}|{self.title}|{self.author}|{self.text}'
+
+	class Meta:
+		verbose_name = 'Заметка'
+		verbose_name_plural = 'Заметки'
--- notepad/main/views.py
+++ notepad/main/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
 
 def index(request):
-	return render(request, "main/index.html")
+	notes = Notes.objects.all()
+	return render(request, "main/index.html", {'notes': notes})
+
+def note(request, ID):
+	notes = Notes.objects.all()
+	noteData = {'noteid': ID, 'title': 'Untitled', 'author': 'Unknown', 'text': '', 'change': 0}
+	for note in notes:
+		note = str(note).split('|')
+		print(note)
+		if note[0] == ID:
+			noteData['title'] = note[1]
+			noteData['author'] = note[2]
+			noteData['text'] = note[3]
+			noteData['change'] = 1
+	return render(request, "main/note.html", noteData)
